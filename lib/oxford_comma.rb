29f7bb0a0405f37f7d43e2a55412a15b215697ef
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 16:03:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,14 @@
 def oxford_comma(array)
-  if array.length >= 3
-    last element = array[array.length-1]
-    last element.prepend("and ")
+  if array.length == 1
+    return array.join()
+  elsif array.length == 2
+    last_element = array[array.length-1]
+    last_element.prepend("and ")
+    return array.join()
+  elsif array.length >= 3
+    last_element = array[array.length-1]
+    last_element.prepend("and ")
+    array[array.length-1] = last_element
   end
   array.join(',')
 end
-
-
